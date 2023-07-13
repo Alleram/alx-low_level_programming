8058ebd76ea9ffea1705dfc62099703d46bf0f83
Alleram <serugoakram@gmail.com>
2023-07-13 14:44:54 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Main - My first C program that prints using a puts function
+ *
+ * Return: If it returns a zero then its a success
+ */
+
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+	return (0);
+}
